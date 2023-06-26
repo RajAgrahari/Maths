e1d78c3b58863f3fbf247557664be84366dbaba9
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-06-26 23:57:39 +0530

    Medium

--- Ncr.cpp
+++ Ncr.cpp
@@ -8,6 +8,7 @@
         for(int i=0;i<=n;i++)
         {
             vector<int>v(i+1,1);
+         
             for(int j=1;j<i;j++)
             {
                 v[j]=(pascal[j]+pascal[j-1])%mod;
